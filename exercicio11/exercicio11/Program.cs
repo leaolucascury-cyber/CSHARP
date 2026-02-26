4ecd6722ca1330599bfee197e307105359160a32
Lucas Cury Leao <1006901@senacminas.edu.br>
2026-02-26 15:23:31 -0300

    Projeto C#

---
+++ exercicio11/exercicio11/Program.cs
@@ -0,0 +1,23 @@
+﻿double a, b, c;
+
+Console.Write("Digite o primeiro lado: ");
+a = double.Parse(Console.ReadLine());
+
+Console.Write("Digite o segundo lado: ");
+b = double.Parse(Console.ReadLine());
+
+Console.Write("Digite o terceiro lado: ");
+c = double.Parse(Console.ReadLine());
+
+if (a == b && b == c)
+{
+    Console.WriteLine("Triângulo Equilátero");
+}
+else if (a == b || a == c || b == c)
+{
+    Console.WriteLine("Triângulo Isósceles");
+}
+else
+{
+    Console.WriteLine("Triângulo Escaleno");
+}
