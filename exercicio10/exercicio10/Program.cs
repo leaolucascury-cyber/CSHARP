4e590e3463334b450d53eeaee2c8cd17e9b6f31f
Lucas Cury Leao <1006901@senacminas.edu.br>
2026-02-26 15:25:17 -0300

    Projeto C#

---
+++ exercicio10/exercicio10/Program.cs
@@ -0,0 +1,23 @@
+﻿int codigo;
+
+Console.Write("Digite um código (1, 2 ou 3): ");
+codigo = int.Parse(Console.ReadLine());
+
+switch (codigo)
+{
+    case 1:
+        Console.WriteLine("Comando Iniciar");
+        break;
+
+    case 2:
+        Console.WriteLine("Comando Pausar");
+        break;
+
+    case 3:
+        Console.WriteLine("Comando Encerrar");
+        break;
+
+    default:
+        Console.WriteLine("Código inválido");
+        break;
+}
