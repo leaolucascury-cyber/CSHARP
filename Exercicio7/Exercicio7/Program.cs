3dccfa04090f7635bd589ff11e35d46a4876a1f4
Lucas Cury Leao <1006901@senacminas.edu.br>
2026-02-26 15:41:18 -0300

    Projeto C#

---
+++ Exercicio7/Exercicio7/Program.cs
@@ -0,0 +1,10 @@
+﻿int a = 15;
+int b = 8;
+int c = 32;
+int temp;
+
+if (a < b) { temp = a; a = b; b = temp; }
+if (a < c) { temp = a; a = c; c = temp; }
+if (b < c) { temp = b; b = c; c = temp; }
+
+Console.WriteLine($"Ordem decrescente: {a}, {b}, {c}");
