48b97c4f21535c70a3bb83b12b5245a5dda90946
Lucas Cury Leao <1006901@senacminas.edu.br>
2026-02-26 15:43:32 -0300

    Projeto C#

---
+++ Exercicio6/Exercicio6/Program.cs
@@ -0,0 +1,15 @@
+﻿double valor;
+
+Console.Write("Digite um número: ");
+valor = double.Parse(Console.ReadLine());
+
+if (valor < 0)
+{
+    valor = valor * -1;
+}
+else
+{
+    valor = valor;
+}
+
+Console.WriteLine("Valor absoluto: " + valor);
