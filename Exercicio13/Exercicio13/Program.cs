4553c88e1f0ff21e575359710e1256ab62327e82
Lucas Cury Leao <1006901@senacminas.edu.br>
2026-02-26 15:20:00 -0300

    Projeto C#

---
+++ Exercicio13/Exercicio13/Program.cs
@@ -0,0 +1,9 @@
+﻿int numero;
+
+Console.Write("Digite um número: ");
+numero = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine(numero + " x " + i + " = " + (numero * i));
+}
