553bc864b53f7f0836f77e55bf0741c723de8f3f
Lucas Cury Leao <1006901@senacminas.edu.br>
2026-02-26 15:15:12 -0300

    Projeto C#

---
+++ Exercico5/Exercico5/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Tempratura dada pelo sensor : ");
+double celcius = double.Parse(Console.ReadLine());
+
+double fahrenheit = ((celcius * 9 / 5) + 32);
+Console.WriteLine($"A temperatura hoje é: {celcius}°, que em fahrenheit é: {fahrenheit:F2}°");
