f6453825f103d202c3666a3899bae308d6a49584
Lucas Cury Leao <1006901@senacminas.edu.br>
2026-02-26 15:27:27 -0300

    Projeto C#

---
+++ exercicio9/exercicio9/Program.cs
@@ -0,0 +1,12 @@
+﻿int valor;
+
+Console.Write("Digite um número: ");
+valor = int.Parse(Console.ReadLine());
+
+if (valor >= 0 && valor <= 9)
+{
+    Console.WriteLine("Valor Válido");
+}
+else
+{
+    Console.WriteLine("Valor Inválido");
